5d5e082815726190c7ea934873beed1c8327231d
Jack McNicol <jack.mcnicol@aligent.com.au>
2021-01-13 12:39:28 +1030

    add initial setup

---
+++ snowpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "mount": {
+    "src": "/built",
+    "public": "/"
+  },
+  "plugins": [
+    [
+      "@snowpack/plugin-babel",
+      {
+        "input": ['.js', '.mjs', '.jsx', '.ts', '.tsx'], // (optional) specify files for Babel to transform
+        transformOptions: {
+          // babel transform options
+        }
+      }
+    ]
+  ]
+}
